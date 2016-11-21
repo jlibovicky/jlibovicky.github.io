51574e7dba887da9884aaf4eed663efb476f5aa9
Jindrich Libovicky <libovicky@ufal.mff.cuni.cz>
2016-11-21 23:22:00 +0100

    directory with code

---
+++ assets/code/punctuation/sentence_split.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+
+import re
+import sys
+
+# patterns to detect stop and start of a document
+DOC_START = re.compile(r"^<doc .*>")
+DOC_END = re.compile(r"^</doc>$")
+
+if __name__ == "__main__":
+    # initialize the sentence splitter
+    sentence_splitter = nltk.data.load("tokenizers/punkt/english.pickle")
+
+    # used for collecting plain text
+    collected_text = ""
+    lines_added = 0
+
+    for line in sys.stdin:
+        line = line.rstrip() # remove \n from the end
+        if not DOC_START.match(line) and not DOC_END.match(line):
+            collected_text += u" " + line
+            lines_added += 1
+
+        if lines_added >= 1000:
+            sentences = sentence_splitter.tokenize(tmp_text)
+            # the last sentence may be unfinished, we leave it for the
+            # the next sentence splitting
+            for sent in sentences[:-1]:
+                print(sent)
+            collected_text = sentences[-1]
+            lines_added = 0
+
+    for sent in sentence_splitter.tokenize(current_text):
+        print(sent)
