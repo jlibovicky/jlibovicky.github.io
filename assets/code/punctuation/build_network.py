76cb63644eeacfadf7e69ef7fd5e2ae99467b29b
Jindrich Libovicky <libovicky@ufal.mff.cuni.cz>
2016-11-23 20:26:02 +0100

    more complicated network and first version train script

--- assets/code/punctuation/build_network.py
+++ assets/code/punctuation/build_network.py
@@ -1,33 +1,41 @@
 import tensorflow as tf
 
+ALPHABET = list(("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+                 "0123456789\"'?!.% #@$(){}[]_"))
 TARGET_CLASSES = ["_", ",", ";"]
 
-
-def build_network(alphabet, max_lenght=200, embedding_size=15, gru_size=64):
+def build_network(max_lenght=300, embedding_size=32, gru_size=256):
     seq_input = tf.placeholder(tf.int32, shape=[None, max_lenght])
+    lengths = tf.placeholder(tf.int64, shape=[None])
+    dropout_plc = tf.placeholder(tf.float32)
 
     embeddings = tf.get_variable(
-        "embeddings", shape=[len(alphabet), embedding_size])
+        "embeddings", shape=[len(ALPHABET), embedding_size])
 
     embedded_sequence = tf.nn.embedding_lookup(embeddings, seq_input)
+    embedded_sequence_dropped = tf.nn.dropout(embedded_sequence, dropout_plc)
 
-    output, _ = tf.nn.dynamic_rnn(
+    (output_fw, output_bg), _ = tf.nn.bidirectional_dynamic_rnn(
         tf.nn.rnn_cell.GRUCell(gru_size),
-        embedded_sequence,
-        dtype=tf.float32
+        tf.nn.rnn_cell.GRUCell(gru_size),
+        embedded_sequence_dropped,
+        dtype=tf.float32,
+        sequence_length=lengths
     )
+    output = tf.concat(2, [output_fw, output_bg])
 
-    weight_matrix = tf.get_variable("W", shape=[gru_size, len(TARGET_CLASSES)])
+    weight_matrix = tf.get_variable("W", shape=[2 * gru_size, len(TARGET_CLASSES)])
     bias = tf.get_variable("b", shape=[len(TARGET_CLASSES)])
-    logits = tf.matmul(tf.reshape(
-        output, shape=[-1, gru_size]), weight_matrix) + bias
+    outputs_reshapes = tf.reshape(output, shape=[-1, 2 * gru_size])
+    outputs_reshapes_dropped = tf.nn.dropout(outputs_reshapes, dropout_plc)
+    logits = tf.matmul(outputs_reshapes_dropped, weight_matrix) + bias
 
     targets = tf.placeholder(tf.int32, shape=[None, max_lenght])
 
-    cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(
-        logits, tf.reshape(targets, shape=[-1]))
+    cross_entropy = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(
+        logits, tf.reshape(targets, shape=[-1])))
 
     predictions = tf.reshape(
         logits, shape=[-1, max_lenght, len(TARGET_CLASSES)])
 
-    return seq_input, targets, predictions, cross_entropy
+    return seq_input, lengths, targets, predictions, cross_entropy, dropout_plc
---
+++ assets/code/punctuation/train.py
@@ -0,0 +1,121 @@
+import itertools
+import re
+import sys
+import numpy as np
+import tensorflow as tf
+from build_network import build_network, ALPHABET, TARGET_CLASSES
+
+MAX_LEN = 200
+ALPHABET_DICT = {char: index for index, char in enumerate(ALPHABET)}
+TARGET_DICT = {clazz: index for index, clazz in enumerate(TARGET_CLASSES)}
+
+
+def data_to_tensor(texts, dictionary):
+    text_indices = []
+    lengths = []
+    for text in texts:
+        text = text.rstrip()
+        indices = [dictionary.get(c, 0) for c in text]
+        lengths.append(min(MAX_LEN, len(text)))
+        if len(indices) > MAX_LEN:
+            text_indices.append(indices[:MAX_LEN])
+        else:
+            text_indices.append(indices +
+                                [0 for _ in range(MAX_LEN - len(indices))])
+    return np.array(text_indices), np.array(lengths)
+
+
+def evaluation(logits, targets, lengths):
+    predicted_classes = np.argmax(logits, axis=2)
+
+    correct = 0.
+    correct_punct = 0.
+    in_prediction = 0.
+    in_annotation = 0.
+
+    for predicted, target, length in zip(predicted_classes, targets, lengths):
+        correct += np.sum(predicted[:length] == target[:length])
+        correct_punct += np.sum((predicted[:length] > 0) & (predicted[:length] == target[:length]))
+        in_prediction += np.sum(predicted[:length] > 0)
+        in_annotation += np.sum(target[:length] > 0)
+
+    accuracy = correct / np.sum(lengths)
+    precision = correct_punct / in_prediction if in_prediction else 0.
+    recall = correct_punct / in_annotation if in_annotation else 0.
+    if precision and recall:
+        f_score = 2 * precision * recall / (precision + recall)
+    else:
+        f_score = 0.
+
+    return (accuracy, precision, recall, f_score)
+
+
+def main():
+    seq_input, lengths, targets, predictions_tf, cross_entropy_tf, dropout_plc = build_network(
+        max_lenght=MAX_LEN)
+    f_text = open(sys.argv[1])
+    f_punct = open(sys.argv[2])
+
+    val_text, val_lengths = data_to_tensor(itertools.islice(f_text, 1000), ALPHABET_DICT)
+    val_punct, _ = data_to_tensor(itertools.islice(f_punct, 1000), TARGET_DICT)
+
+    reg_losses = tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES)
+    reg_constant = 0.01  # Choose an appropriate one.
+
+    bias_regex = re.compile(r'[Bb]ias')
+    regularizable = [tf.reduce_sum(
+        v ** 2) for v in tf.trainable_variables()
+                     if bias_regex.findall(v.name)]
+
+    l2_value = sum(v * v for v in regularizable)
+    l2_cost = 1e-6 * l2_value
+
+    cost = cross_entropy_tf + l2_cost
+    optimizer = tf.train.AdamOptimizer(1e-4)
+    train_op = optimizer.minimize(cost)
+
+    session = tf.Session()
+    session.run(tf.initialize_all_variables())
+
+    batch_n = 0
+
+    while True:
+        batch_n += 1
+        text_batch, batch_lengths = data_to_tensor(itertools.islice(f_text, 64), ALPHABET_DICT)
+        punct_batch, _ = data_to_tensor(itertools.islice(f_punct, 64), TARGET_DICT)
+
+        if text_batch.shape == (0,):
+            break
+
+        _, predictions, cross_entropy = session.run(
+            [train_op, predictions_tf, cross_entropy_tf],
+            feed_dict={seq_input: text_batch,
+                       targets: punct_batch,
+                       lengths: batch_lengths,
+                       dropout_plc: 0.5})
+        accuracy, precision, recall, f_score = evaluation(predictions, punct_batch, batch_lengths)
+        print("batch {}:\tacc: {:.4f}\tprec: {:.4f}\trecall: {:.4f}\tF1: {:.4f}\txent: {:.4f}".format(
+            batch_n, accuracy, precision, recall, f_score, cross_entropy))
+
+        if batch_n % 20 == 0:
+            predictions, cross_entropy = session.run(
+                [predictions_tf, cross_entropy_tf],
+                feed_dict={seq_input: val_text,
+                           targets: val_punct,
+                           lengths: val_lengths,
+                           dropout_plc: 1.0})
+            accuracy, precision, recall, f_score = evaluation(predictions, punct_batch, batch_lengths)
+            print("batch {}:\tacc: {:.4f}\tprec: {:.4f}\trecall: {:.4f}\txent: {:.4f}".format(
+                batch_n, accuracy, precision, recall, f_score, cross_entropy))
+
+            print("")
+            print("Valdidation after batch {}".format(batch_n))
+            print("acc: {:.4f}\tprec: {:.4f}\trecall: {:.4f}\txent: {:.4f}".format(
+                accuracy, precision, recall, f_score, cross_entropy))
+            print("")
+
+    f_text.close()
+    f_punct.close()
+
+if __name__ == "__main__":
+    main()
